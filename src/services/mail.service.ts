4f29af2bc87a83b5146ab04d857a828f8db6b95d
fahad <fahad@mpower-social.com>
2025-10-07 12:41:19 +0600

    ssl true in gmail

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -7,7 +7,7 @@ export class MailService {
     this.transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST || "smtp.gmail.com",
       port: Number(process.env.SMTP_PORT) || 587,
-      secure: false, // true for 465
+      secure: true, // true for 465
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
